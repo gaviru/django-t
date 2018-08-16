d845afe28a01b64ccb2dd0aa6851f7a203bafb42
gaviru <gavirupp@gmail.com>
2018-08-16 15:31:23 +0900

    post 게시판
    페이징 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import ListView
+from django.core.paginator import Paginator
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -6,13 +8,55 @@ from utils.str_util import StrUtil
 
 # Create your views here.
 
-def post_list(request):
-    qs = Post.objects.filter(published_date__lte=timezone.now()
-                    ).order_by('published_date')
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = 'postL'
+    paginate_by = 10
 
-    return render(request, 'blog/post_list.html',{
-        'postL': qs,
-    })
+    def get_context_data(self, **kwargs):
+        context = super(PostListView, self).get_context_data(**kwargs)
+        paginator = context['paginator']
+        page_numbers_range = 5  # Display only 5 page numbers
+        max_index = len(paginator.page_range)
+
+        page = self.request.GET.get('page')
+        current_page = int(page) if page else 1
+
+        start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
+        end_index = start_index + page_numbers_range
+        if end_index >= max_index:
+            end_index = max_index
+
+        page_range = paginator.page_range[start_index:end_index]
+        context['page_range'] = page_range
+        return context
+
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()
+                            ).order_by('-published_date')
+
+# def post_list(request):
+#     qs = Post.objects.filter(published_date__lte=timezone.now()
+#                     ).order_by('-published_date')
+
+#     PostL = get_context_data(qs)
+
+#     # paginator = Paginator(qs, 5) # Show 5 PostL per page
+#     # page = request.GET.get('page')
+#     # try:
+#     #     PostL = paginator.page(page)
+#     # except PageNotAnInteger:
+#     #     # If page is not an integer, deliver first page.
+#     #     PostL = paginator.page(1)
+#     # except EmptyPage:
+#     #     # If page is out of range (e.g. 9999), deliver last page of results.
+#     #     PostL = paginator.page(paginator.num_pages)
+
+#     return render(request, 'blog/post_list.html',{
+#         'postL': qs,
+#     })
 
 def post_detail(request, pk):
     postObj = get_object_or_404(Post, pk=pk)
@@ -58,3 +102,22 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {
         'form': form,
     })
+
+
+def get_context_data(self, **kwargs):
+    context = super(views, self).get_context_data(**kwargs)
+    paginator = context['paginator']
+    page_numbers_range = 10 
+    max_index = len(paginator.page_range)
+
+    page = self.request.GET.get('page')
+    current_page = int(page) if page else 1
+
+    start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
+    end_index = start_index + page_numbers_range
+    if end_index >= max_index:
+        end_index = max_index
+
+    page_range = paginator.page_range[start_index:end_index]
+    context['page_range'] = page_range
+    return context
